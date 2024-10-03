7f73b8c6d973e4371f51950fa34c98df4d2ca925
ekachaikeaw <ekachai.keawman@gmail.com>
2024-10-03 17:42:37 +0700

    add: add wraper func create mockDB

--- ecomm-api/storer/storer_mysql_test.go
+++ ecomm-api/storer/storer_mysql_test.go
@@ -2,7 +2,7 @@ package storer
 
 import (
 	"context"
-	
+
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -10,8 +10,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateProduct(t *testing.T) {
-	// some test
+func withTestDB(t *testing.T, fn func(*sqlx.DB, sqlmock.Sqlmock)) {
 	mockDB, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
 	if err != nil {
 		t.Fatalf("error creating mock database: %v", err)
@@ -19,58 +18,59 @@ func TestCreateProduct(t *testing.T) {
 	defer mockDB.Close()
 
 	db := sqlx.NewDb(mockDB, "sqlmock")
-	st := NewMySQLStorer(db)
+	fn(db, mock)
+}
+func TestCreateProduct(t *testing.T) {
+	withTestDB(t, func(db *sqlx.DB, mock sqlmock.Sqlmock) {
+		st := NewMySQLStorer(db)
 
-	p := &Product{
-		Name: "test product",
-		Image: "test.jpg",
-		Category: "test category",
-		Description: "test description",
-		Rating: 5,
-		NumReviews: 10,
-		Price: 100.0,
-		CountInStock: 100,
-	}
+		p := &Product{
+			Name:         "test product",
+			Image:        "test.jpg",
+			Category:     "test category",
+			Description:  "test description",
+			Rating:       5,
+			NumReviews:   10,
+			Price:        100.0,
+			CountInStock: 100,
+		}
 
-	mock.ExpectExec("INSERT INTO products (name, image, category, description, rating, num_reviews, price, count_in_stock) VALUES (?, ?, ?, ?, ?, ?, ?, ?)").WillReturnResult(sqlmock.NewResult(1, 1))
-	cp, err := st.CreateProduct(context.Background(), p)
-	require.NoError(t, err)
-	require.Equal(t, int64(1), cp.ID)
-	err = mock.ExpectationsWereMet()
-	require.NoError(t, err)
+		mock.ExpectExec("INSERT INTO products (name, image, category, description, rating, num_reviews, price, count_in_stock) VALUES (?, ?, ?, ?, ?, ?, ?, ?)").WillReturnResult(sqlmock.NewResult(1, 1))
+		cp, err := st.CreateProduct(context.Background(), p)
+		require.NoError(t, err)
+		require.Equal(t, int64(1), cp.ID)
+		err = mock.ExpectationsWereMet()
+		require.NoError(t, err)
+	})
+
 }
 
 func TestGetProduct(t *testing.T) {
-	mockDB, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
-	if err != nil {
-		t.Fatalf("error creating mock database: %v", err)
-	}
-	defer mockDB.Close()
+	withTestDB(t, func(db *sqlx.DB, mock sqlmock.Sqlmock) {
+		st := NewMySQLStorer(db)
 
-	db := sqlx.NewDb(mockDB, "sqlmock")
-	st := NewMySQLStorer(db)
+		p := &Product{
+			Name:         "test product",
+			Image:        "test.jpg",
+			Category:     "test category",
+			Description:  "test description",
+			Rating:       5,
+			NumReviews:   10,
+			Price:        100.0,
+			CountInStock: 100,
+		}
 
-	p := &Product{
-		Name: "test product",
-		Image: "test.jpg",
-		Category: "test category",
-		Description: "test description",
-		Rating: 5,
-		NumReviews: 10,
-		Price: 100.0,
-		CountInStock: 100,
-	}
+		rows := sqlmock.NewRows([]string{"id", "name", "image", "category", "description", "rating", "num_reviews", "price", "count_in_stock", "created_at", "updated_at"}).
+			AddRow(1, p.Name, p.Image, p.Category, p.Description, p.Rating, p.NumReviews, p.Price, p.CountInStock, p.CreatedAt, p.UpdatedAt)
 
-	rows := sqlmock.NewRows([]string{"id", "name", "image", "category", "description", "rating", "num_reviews", "price", "count_in_stock", "created_at", "updated_at"}).
-		AddRow(1, p.Name, p.Image, p.Category, p.Description, p.Rating, p.NumReviews, p.Price, p.CountInStock, p.CreatedAt, p.UpdatedAt)
-	
-	mock.ExpectQuery("SELECT * FROM products WHERE id=?").WithArgs(1).WillReturnRows(rows)
+		mock.ExpectQuery("SELECT * FROM products WHERE id=?").WithArgs(1).WillReturnRows(rows)
 
-	gp, err := st.GetProduct(context.Background(), 1)
-	require.NoError(t, err)
-	require.Equal(t, int64(1), gp.ID)
+		gp, err := st.GetProduct(context.Background(), 1)
+		require.NoError(t, err)
+		require.Equal(t, int64(1), gp.ID)
 
-	err = mock.ExpectationsWereMet()
-	require.NoError(t, err)
-	
+		err = mock.ExpectationsWereMet()
+		require.NoError(t, err)
+	})
+
 }
