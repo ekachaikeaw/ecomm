44e94fbe0892d74994117381d5e13fb643f6a166
ekachaikeaw <ekachai.keawman@gmail.com>
2024-10-18 14:52:45 +0700

    add: add CreateToken method

--- token/jwt_maker.go
+++ token/jwt_maker.go
@@ -1,6 +1,7 @@
 package token
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -15,5 +16,15 @@ func NewJWTMaker(secretKey string) *JWTMaker {
 }
 
 func (maker *JWTMaker) CreateToken(id int64, email string, isAdmin bool, duration time.Duration) (string, *UserClaims, error) {
-	jwt.NewWithClaims(jwt.SigningMethodES256, )
+	claims, err := NewUserClaims(id, email, isAdmin, duration)
+	if err != nil {
+		return "", nil, err
+	}
+  	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	tokenStr, err := token.SignedString([]byte(maker.secretKey))
+	if err != nil {
+		return "", nil, fmt.Errorf("error signing token: %w", err)
+	}
+
+	return tokenStr, claims, nil
 }
