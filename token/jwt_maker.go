170284ec4baade5df52a98dac848e55b27b22043
ekachaikeaw <ekachai.keawman@gmail.com>
2024-10-18 15:26:04 +0700

    add: VertifyToken method

--- token/jwt_maker.go
+++ token/jwt_maker.go
@@ -27,4 +27,27 @@ func (maker *JWTMaker) CreateToken(id int64, email string, isAdmin bool, duratio
 	}
 
 	return tokenStr, claims, nil
+} 
+
+func (maker *JWTMaker) VerifyToken(tokenStr string) (*UserClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &UserClaims{}, func(token *jwt.Token) (interface{}, error) {
+		// verify signing method
+
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, fmt.Errorf("invalid token signing method")
+		}
+		
+		return []byte(maker.secretKey), nil 
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error parsing token: %w", err)
+	}
+
+	claims, ok := token.Claims.(*UserClaims)
+	if !ok {
+		return nil, fmt.Errorf("invalid token claims")
+	}
+
+	return claims, nil
 }
